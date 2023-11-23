41571d285412a473078ff1aa393839c948a5e7df
Nick Pueschel <npueschel@gmail.com>
2023-11-22 19:59:23 -0500

    swipe controls

--- skull.js
+++ skull.js
@@ -4,6 +4,11 @@ function initializeSkull() {
     window.skull.end = 184;
     window.skull.current = 153;
     document.onkeydown = checkKey;
+    document.addEventListener('touchstart', handleTouchStart, false);
+    document.addEventListener('touchmove', handleTouchMove, false);
+
+    window.skull.xDown = null;
+    window.skull.yDown = null;
 }
 
 function checkKey(e) {
@@ -26,6 +31,52 @@ function checkKey(e) {
         changeDirection("up");
     }
 }
+
+
+
+function handleTouchStart(evt) {
+    //window.skull.xDown = evt.originalEvent.touches[0].clientX;
+    //window.skull.yDown = evt.originalEvent.touches[0].clientY;
+
+    window.skull.xDown = evt.touches[0].clientX;
+    window.skull.yDown = evt.touches[0].clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!window.skull.xDown || !window.skull.yDown) {
+        return;
+    }
+
+    //var xUp = evt.originalEvent.touches[0].clientX;
+   // var yUp = evt.originalEvent.touches[0].clientY;
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = window.skull.xDown - xUp;
+    var yDiff = window.skull.yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        if (xDiff > 0) {
+            /* left swipe */
+            changeDirection("down");
+        } else {
+            /* right swipe */
+            changeDirection("up");
+        }
+    } else {
+        if (yDiff > 0) {
+            /* up swipe */
+            changeDirection("up");
+        } else {
+            /* down swipe */
+            changeDirection("down");
+        }
+    }
+    /* reset values */
+    window.skull.xDown = null;
+    window.skull.yDown = null;
+};
 
 function changeDirection(dir) {
     document.getElementById("skullNotification").innerHTML = "";
